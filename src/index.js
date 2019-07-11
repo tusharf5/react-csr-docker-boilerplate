b9b4d8c6632034a9eacf7739c986b27cf8e21563
Tushar Sharma <ts17995@gmail.com>
2019-07-12 01:22:37 +0530

    refactor: cleanup unnecessary comments and stlyles

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Root from './Root';
-import './styles/main.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-require('./favicon.ico'); // Tell webpack to load favicon.ico
+import './styles/main.scss';
+require('./favicon.ico');
 
 const history = {};
 
